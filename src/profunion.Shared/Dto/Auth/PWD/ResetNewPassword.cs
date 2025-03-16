0f4dca4cfb972a452097d24df9a7beae097ac42b
Pavel Repetiy <repetiy.pavel@mail.ru>
2025-03-16 21:22:09 +0300

    add_Dto_pwd

---
+++ src/profunion.Shared/Dto/Auth/PWD/ResetNewPassword.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace profunion.Shared.Dto.Auth.PWD
+{
+    public class ResetNewPassword
+    {
+        public string? newPassword { get; set; }
+    }
+}
